2c882c3a21af707969ed392caf9e32b99f376f8c
danh <dahorrell@gmail.com>
2024-04-22 19:44:17 -0400

    Make warcraft logs a bit more user friendly

--- wlsf.user.js
+++ wlsf.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Warcraftlogs - Hide bottom sticky footer
-// @version      1.2
+// @version      2.1
 // @author       danh
 // @description  Remove the bottom banner on Warcraftlogs for a better user experience
 // @match        https://www.warcraftlogs.com/*
@@ -10,14 +10,29 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    setInterval(function() {
-        const footer = document.getElementsByClassName("ad-placement-sticky-footer ad-placement-sticky-footer--playwire");
-		if (footer != null) {
-			footer[0].style = "display: none;";
+    var footer = document.getElementById("content").children[3].children;
+	if (footer != null) {
+		footer[0].style = "display: none;";
+	}
+	var profileAdd = document.getElementById("gear-box-ad");
+	if (profileAdd != null) {
+		profileAdd.style = "display: none;";
+	}
+	var sideAdd = document.getElementById("tile-content");
+	if (sideAdd != null) {
+		var si = sideAdd.parentElement.children[1].children[0];
+		if (si != null) {
+			si.style = "display: none;"
 		}
-		const profileAdd = document.getElementById("gear-box-ad");
-		if (profileAdd != null) {
-			profileAdd.style = "display: none;";
+	}
+	var side = document.getElementById("right-vertical-banner");
+	if (side != null) {
+		side.style = "display: none;"
+	}
+	var rvb = document.getElementById("content");
+	if (rvb) {
+		if (rvb.children.length == 10) {
+			rvb.children[8].style = "display: none;"
 		}
-    }, 10)();
+	}
 })();
